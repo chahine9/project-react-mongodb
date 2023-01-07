2f869d8561d11977609f56c33e243c3f705ebb6a
rayenfassatoui <r.fasatoui20081@pi.tn>
2023-01-06 23:17:06 -0800

    version final

--- front/app/src/components/Navbar.js
+++ front/app/src/components/Navbar.js
@@ -16,13 +16,13 @@ export default function NavBar(){
   
             <a className="nav-link nav-profile d-flex align-items-center pe-0" href="#" data-bs-toggle="dropdown">
               <img src="template/assets/img/profile-img.jpg" alt="Profile" className="rounded-circle" />
-              <span className="d-none d-md-block dropdown-toggle ps-2">K. Anderson</span>
+              <span className="d-none d-md-block dropdown-toggle ps-2">rayen fassatoui</span>
             </a> 
   
             <ul className="dropdown-menu dropdown-menu-end dropdown-menu-arrow profile">
               <li className="dropdown-header">
-                <h6>Kevin Anderson</h6>
-                <span>Web Designer</span>
+                <h6>rayen fassatoui</h6>
+        
               </li>
               <li>
                 <hr className="dropdown-divider" />
